7e637ee041530068360d2ca7ac552b9b270270fb
Sudhanshu Pandey <57751726+sudhanshuvlog@users.noreply.github.com>
2024-07-06 20:54:32 +0530

    CommandHistory-6July.sh

---
+++ Linux/CommandHistory-6July.sh
@@ -0,0 +1,212 @@
+ 1  cd /
+    2  ls
+    3  clear
+    4  whoami
+    5  useradd dev1
+    6  id root
+    7  id dev1
+    8  id ram
+    9  useradd ram
+   10  id ram
+   11  cd /home/
+   12  ls
+   13  pwd
+   14  cat /etc/host
+   15  cat /etc/hosts
+   16  cat /etc/group
+   17  groupadd devs
+   18  groupadd qaengineer
+   19  groupadd devops
+   20  groups managers
+   21  groupadd managers
+   22  cat /etc/group
+   23  useradd --help
+   24  useradd -G devs dev2
+   25  cat /etc/group
+   26  id dev2
+   27  clear
+   28  history
+   29  id dev1
+   30  usermod -G devs dev1
+   31  id dev1
+   32  cat /etc/group
+   33  clear
+   34  ls
+   35  passwd dev1
+   36  su dev1
+   37  ls
+   38  cd dev1
+   39  ls
+   40  cat a.txt 
+   41  useradd qa1
+   42  passwd qa1
+   43  su qa1
+   44  passwd dev2
+   45  id dev2
+   46  su dev2
+   47  clear
+   48  passwd root
+   49  clear
+   50  cd dev1
+   51  su dev1
+   52  whoami
+   53  yum install httpd
+   54  cat /etc/shadow
+   55  clear
+   56  vi /etc/sudoers
+   57  vi /etc/sudoers
+   58  su dev1
+   59  vi /etc/sudoers
+   60  su dev1
+   61  vi /etc/sudoers
+   62  su dev2
+   63  vi /etc/sudoers
+   64  su dev2
+   65  vi /etc/sudoers
+   66  which yum
+   67  vi /etc/sudoers
+   68  su dev2
+   69  vi /etc/sudoers
+   70  clear
+   71  yum install httpd
+   72  cd /etc/yum.repos.d/
+   73  pwd
+   74  ls
+   75  vi amazonlinux.repo 
+   76  yum search docker
+   77  yum search docker23213234
+   78  yum info docker
+   79  yum update
+   80  yum update httpd
+   81  yum remove httpd
+   82  rpm -q httpd
+   83  yum list installed
+   84  yum clean all
+   85  yum repolist
+   86  yum history
+   87  ls
+   88  vi epel.repo
+   89  yum repolist
+   90  yum search binutils
+   91  yum install binutils-aarch64-linux-gnu.x86_64
+   92  yum whatprovides httpd
+   93  clear
+   94  vi a.sh
+   95  pwd
+   96  mv a.sh /shell-scripts
+   97  cd /shell-sscripts
+   98  cd /
+   99  ls
+  100  cp shell-scripts /shell-scripts/
+  101  mkdir shell-scripts
+  102  ls
+  103  mkdir shell-scripts-gfg
+  104  mv shell-scripts /shell-scripts-gfg/
+  105  ls
+  106  cd shell-scripts-gfg/
+  107  ls
+  108  mv shell-scripts a.sh
+  109  cat a.sh 
+  110  bash a.sh
+  111  ls
+  112  vi a.sh 
+  113  vi b.sh
+  114  bash b.sh 
+  115  vi b.sh
+  116  clear
+  117  bash b.sh 
+  118  bash b.sh sudhanshu
+  119  bash b.sh gfg
+  120  cat b.sh 
+  121  bash b.sh gfg sudhanshu
+  122* 
+  123  vi b.sh
+  124  clear
+  125  bash b.sh gfg sudhanshu
+  126  bash b.sh gfg sudhanshu ram
+  127  vi b.sh
+  128  bash b.sh gfg sudhanshu ram
+  129  vi b.sh
+  130  clear
+  131  bash b.sh rohan
+  132  id rohan
+  133  id rohan123
+  134  bash b.sh rohan123
+  135  id rohan123
+  136  bash b.sh rohan123
+  137  vi b,sh
+  138  vi b.sh
+  139  bash b.sh rohan123
+  140  bash b.sh rohan123 ram shayam gfg sudhanshu
+  141  bash b.sh rohan123
+  142  vi b.sh
+  143  bash b.sh gfg sudhanshu 
+  144  vi b.sh
+  145  bash b.sh gfg
+  146  id gfg
+  147  bash b.sh gfg
+  148  vi b.sh
+  149  clear
+  150  id ram
+  151  bash b.sh sudhanshu
+  152  cat b.sh
+  153  bash b.sh sudhanshu
+  154  clear
+  155  date
+  156  date > output.txt
+  157  cat output.txt 
+  158  cal
+  159  cal > output.txt
+  160  cat output.txt 
+  161  bash b.sh sudhanshu
+  162  cal > /dev/null
+  163  vi b.sh
+  164  bash b.sh sudhanshu
+  165  bash b.sh sudhanshu123
+  166  cal > output.txt
+  167  cal123 > output.txt
+  168  cal123 &> output.txt
+  169  cal &> output.txt
+  170  vi b.sh
+  171  clear
+  172  ls
+  173  mv b.sh user-create.sh
+  174  bash user-create.sh sudhanshu
+  175  vi user-create.sh 
+  176  bash user-create.sh sudhanshu
+  177  bash user-create.sh sudhanshu432435353
+  178  vi user-create.sh 
+  179  clear
+  180  cal123 &> /dev/null
+  181  cal &> /dev/null
+  182  echo $?
+  183  date
+  184  echo $?
+  185  date123
+  186  echo $?
+  187  cal
+  188  echo $?
+  189  vi user-create.sh 
+  190  clear
+  191  bash user-create.sh sudhanshu
+  192  bash user-create.sh digvijay
+  193  vi user-create.sh 
+  194  bash user-create.sh digvijay
+  195  bash user-create.sh arjun
+  196  vi user-db.csv
+  197  ls
+  198  vi user-create.sh 
+  199  cat user-db
+  200  cat user-db.csv 
+  201  clear
+  202  bash user-create.sh 
+  203  vi c.sh
+  204  bash c.sh 
+  205  which bash
+  206  vi user-
+  207  vi user-create.sh 
+  208  ls
+  209  chmod +x user-create.sh 
+  210  ls
+  211  ./user-create.sh 
+  212  history
